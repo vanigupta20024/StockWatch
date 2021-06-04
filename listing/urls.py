885ce65c09d555fee74af5f52595c3e0df1b40c0
Vani Gupta <topcoder69@gmail.com>
2021-06-04 15:24:07 +0530

    updated wsgi

--- listing/urls.py
+++ listing/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.listing, name = "listing"),
     path('stock_info/', include('stock_info.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- stock_info/urls.py
+++ stock_info/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<str:stock_name>/', views.stock_info, name = "stock_info"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
